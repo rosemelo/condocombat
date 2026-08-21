1b54caf0d4ad0d1843f5cc64fa4ea9abcbf1daf4
rosemelo <rosenildesmelo@gmail.com>
2026-08-21 08:52:43 -0300

    fix: separa tag da imagem docker no render e altera busca netlify para site_id

--- terraform/landing.tf
+++ terraform/landing.tf
@@ -1,9 +1,7 @@
-# Busca a referência do site da Landing Page na Netlify
 data "netlify_site" "landing" {
-  name = var.netlify_site_name
+  site_id = var.netlify_site_id
 }
 
-# Configura a variável de ambiente PUBLIC_APP_URL com a URL gerada para o Frontend (Render)
 resource "netlify_environment_variable" "landing_public_url" {
   site_id = data.netlify_site.landing.id
   key     = "PUBLIC_APP_URL"
@@ -15,7 +13,6 @@ resource "netlify_environment_variable" "landing_public_url" {
   ]
 }
 
-# Fazer o deploy dos arquivos compilados (landing/dist) gerados no CI via Terraform
 resource "terraform_data" "landing_deploy" {
   triggers_replace = [
     data.netlify_site.landing.id,
